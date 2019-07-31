67cd9356c0be3f1aa300def5c69d6bb03024d683
Jiahao <wang_jiahao@hotmail.com>
2019-07-31 12:45:23 +0800

    second commit

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,4 +28,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  file { '/root/README.md':
+        ensure  => file,
+        content => 'This should exist',
+        owner   => 'root',
+    }
 }
